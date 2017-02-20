da81e8616bb724009e81edfd3b0c469e963d6d24
Dustnblack <dustnblack@gmail.com>
2017-02-19 21:14:44 -0500

    Add files via upload

---
+++ _site/assets/Main-jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
+
+
+
+});
